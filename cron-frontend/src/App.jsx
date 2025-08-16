eeb1034c57fcf254c0e237b132672b5071ddffb3
Jocsã <jocsa.dev@gmail.com>
2025-08-16 19:59:56 -0300

    feat: initial page for frontend

--- cron-frontend/src/App.jsx
+++ cron-frontend/src/App.jsx
@@ -1,10 +1,22 @@
-function App() {
+import { Container, Typography, Paper, Divider, Box } from "@mui/material";
+import CronManager from "./components/CronManager";
 
+function App() {
   return (
-    <>
-      <h1>oi</h1>
-    </>
-  )
+    <Container maxWidth="md" sx={{ paddingY: 4 }}>
+      <Paper sx={{ padding: 3, boxShadow: 3 }}>
+        <Typography variant="h4" align="center" gutterBottom>
+          Gestão de CRONs
+        </Typography>
+
+        <Divider sx={{ marginY: 2 }} />
+
+        <Box>
+          <CronManager />
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
 
-export default App
+export default App;
